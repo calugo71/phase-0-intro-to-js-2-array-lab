912adf3d98449383f8b9693f1fe30b602637021c
Carlos Lugo <calugo71@gmail.com>
2022-07-20 04:37:06 -0400

    commit tree 2b5cf054561724c77f4f455fd90bf43afe606c9e

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+const cats = [
+    "Milo",
+    "Otis",
+    "Garfield",
+]
+const copyOfCats = [...cats]
+function destructivelyAppendCat(name){
+    cats.push("Ralph",);
+    return cats;
+}
+function destructivelyPrependCat(name){
+    cats.unshift("Bob",);
+    return cats;
+}
+function destructivelyRemoveLastCat(name){
+    cats.pop(" ")
+    return cats;
+}
+function destructivelyRemoveFirstCat(name){
+    cats.shift("")
+    return cats
+}
+function appendCat(name){
+   return [...cats, "Broom"]
+}
+function prependCat(name){
+    return ["Arnold", ...cats]
+}
+function removeLastCat(){
+    return [...cats.slice(0, 2),]
+}
+function removeFirstCat(){
+    return [...cats.slice(1),]
+}
